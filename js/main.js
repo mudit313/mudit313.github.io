135ff88c9edc262b35a8b846f6cb23d028a1767b
Mudit Agrawal <mudit.agrawal@nutanix.com>
2020-02-16 20:09:22 +0530

    Fix after including project broke website

--- js/main.js
+++ js/main.js
@@ -31,10 +31,6 @@ $(document).ready(function () {
     $('.panel-cover').addClass('panel-cover--collapsed')
   }
 
-  if (window.location.pathname !== '{{ site.baseurl }}' && window.location.pathname !== '{{ site.baseurl }}index.html') {
-    $('.panel-cover').addClass('panel-cover--collapsed')
-  }
-
   $('.btn-mobile-menu').click(function () {
     $('.navigation-wrapper').toggleClass('visible animated bounceInDown')
     $('.btn-mobile-menu__icon').toggleClass('icon-list icon-x-circle animated fadeIn')
@@ -49,14 +45,14 @@ $(document).ready(function () {
 
 
   $('a.project-button').click(function (e) {
-//    var par = $('.content-wrapper__inner').children();
-//    for(var i=0; i<par.length; i++){
-//        console.log(par[i])
-//        console.log($(par[i]).hasClass('hidden'));
-//        if (!$(par[i]).hasClass('hidden'))
-//            $(par[i]).addClass('hidden');
-//    }
-//    $('.project-list').parent().removeClass('hidden');
+    var par = $('.content-wrapper__inner').children();
+    for(var i=0; i<par.length; i++){
+        console.log(par[i])
+        console.log($(par[i]).hasClass('hidden'));
+        if (!$(par[i]).hasClass('hidden'))
+            $(par[i]).addClass('hidden');
+    }
+    $('.project-list').parent().removeClass('hidden');
 
     if ($('.panel-cover').hasClass('panel-cover--collapsed')) return
     currentWidth = $('.panel-cover').width()
@@ -69,13 +65,13 @@ $(document).ready(function () {
     }
   })
 
-//  if (window.location.hash && window.location.hash == '#project') {
-//    $('.panel-cover').addClass('panel-cover--collapsed')
-//  }
-//
-//  $('.navigation-wrapper .project-button').click(function () {
-//    $('.navigation-wrapper').toggleClass('visible')
-//    $('.btn-mobile-menu__icon').toggleClass('icon-list icon-x-circle animated fadeIn')
-//  })
+  if (window.location.hash && window.location.hash == '#project') {
+    $('.panel-cover').addClass('panel-cover--collapsed')
+  }
+
+  $('.navigation-wrapper .project-button').click(function () {
+    $('.navigation-wrapper').toggleClass('visible')
+    $('.btn-mobile-menu__icon').toggleClass('icon-list icon-x-circle animated fadeIn')
+  })
 
 })
